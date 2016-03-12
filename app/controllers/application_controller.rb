43b736488022c7f69544937e66d79232347552de
Dan Cyr <danielmcyr@gmail.com>
2016-03-12 11:42:32 -0500

    another note to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "hello, world! I am on heroku!"
+    render text: "hello, world! I am on heroku! Again"
   end
 end
